d0d6720826b586c53fd258545411f10899f9ed2c
Tom Haddon <tom.haddon@canonical.com>
2024-06-19 12:33:07 +0200

    Fix integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -15,7 +15,7 @@ from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text(encoding="utf-8"))
+CHARMCRAFT = yaml.safe_load(Path("./charmcraft.yaml").read_text(encoding="utf-8"))
 APP_NAME = METADATA["name"]
 
 
@@ -26,12 +26,10 @@ async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
     Assert on the unit status before any relations/configurations take place.
     """
     # Deploy the charm and wait for active/idle status
-    charm = pytestconfig.getoption("--charm-file")
-    resources = {"httpbin-image": METADATA["resources"]["httpbin-image"]["upstream-source"]}
     assert ops_test.model
     await asyncio.gather(
         ops_test.model.deploy(
-            f"./{charm}", resources=resources, application_name=APP_NAME, series="jammy"
+            f"./{charm}", application_name=APP_NAME, series="noble"
         ),
         ops_test.model.wait_for_idle(
             apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
