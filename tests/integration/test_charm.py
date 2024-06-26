b1cf2e81bdfe1cec1ce93cefc317b9b63cc02229
mthaddon <tom.haddon@canonical.com>
2024-06-26 09:50:36 +0200

    Remove templated metadata.yaml and populate initial charmcraft yaml (#2)
    * Remove templated metadata.yaml and populate initial charmcraft yaml so that basic charm can be built
    
    * Updates for lint and unit tests to pass
    
    * Fix integration tests
    
    * Update variable name in integration tests
    
    * We need to define the charm file in integration test
    
    * Update formatting
    
    * Run integration test on juju 3.3 since we're deploying noble
    
    * Switch to canonical k8s which supports juju 3.3
    
    * Use strictly confined microk8s
    
    * Noble isn't supported on 3.3, bump to 3.4
    
    * Match pylibjuju version to juju version for noble support
    
    * Remove explicit reference to noble in charm deploy

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -15,8 +15,8 @@ from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text(encoding="utf-8"))
-APP_NAME = METADATA["name"]
+CHARMCRAFT = yaml.safe_load(Path("./charmcraft.yaml").read_text(encoding="utf-8"))
+APP_NAME = CHARMCRAFT["name"]
 
 
 @pytest.mark.abort_on_fail
@@ -25,14 +25,11 @@ async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
 
     Assert on the unit status before any relations/configurations take place.
     """
-    # Deploy the charm and wait for active/idle status
     charm = pytestconfig.getoption("--charm-file")
-    resources = {"httpbin-image": METADATA["resources"]["httpbin-image"]["upstream-source"]}
+    # Deploy the charm and wait for active/idle status
     assert ops_test.model
     await asyncio.gather(
-        ops_test.model.deploy(
-            f"./{charm}", resources=resources, application_name=APP_NAME, series="jammy"
-        ),
+        ops_test.model.deploy(f"./{charm}", application_name=APP_NAME),
         ops_test.model.wait_for_idle(
             apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
         ),
