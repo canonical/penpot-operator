e041d703a735564992806edd2fbe2d2c514449a5
Weii Wang <weii.wang@canonical.com>
2024-10-14 15:33:46 +0000

    Update dashboard

--- src/charm.py
+++ src/charm.py
@@ -28,7 +28,8 @@ from charms.traefik_k8s.v2.ingress import IngressPerAppRequirer
 
 logger = logging.getLogger(__name__)
 
-
+# needed for charm libraries
+# pylint: disable=too-many-instance-attributes
 class PenpotCharm(ops.CharmBase):
     """Charm the service."""
 
