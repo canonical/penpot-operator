12e29b5a1e4a75abc806858fe21fdf95cb8508ba
Fouad Atwi <fouad.atwi@canonical.com>
2026-03-05 14:00:26 +0100

    fix unreacheble endpoint

--- tests/integration/prepare.sh
+++ tests/integration/prepare.sh
@@ -5,6 +5,8 @@
 
 set -exo pipefail
 
-sudo bash -c "echo 127.0.0.1 penpot.local >> /etc/hosts"
+ingress_ip="$(k8s kubectl -n ingress get pod -l name=nginx-ingress-microk8s --no-headers -o custom-columns=IP:.status.podIP | head -n1)"
+
+sudo bash -c "echo $ingress_ip penpot.local >> /etc/hosts"
 
 tox -e playwright-install
