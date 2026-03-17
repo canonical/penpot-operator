440729e0a96beba2d73409ff2e639f4dc2af5954
Fouad Atwi <fouad.atwi@canonical.com>
2026-03-17 10:12:13 +0100

    revert conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,15 @@
-# Copyright 2026 Canonical Ltd.
+# Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 """Fixtures for charm tests."""
 
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    import pytest
+def pytest_addoption(parser):
+    """Parse additional pytest options.
 
-
-def pytest_addoption(parser: "pytest.Parser"):
-    """Parse additional pytest options."""
+    Args:
+        parser: Pytest parser.
+    """
     parser.addoption("--charm-file", action="store")
     parser.addoption("--kube-config", action="store")
     parser.addoption("--penpot-image", action="store")
